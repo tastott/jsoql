70e4f1216076ec57667389e0e17540b2a963fcad
Tim Stott <ta.stott@googlemail.com>
2015-04-06 10:28:03 +0100

    Moved some code out of app.ts

---
+++ Code/Jql/Scripts/parse.ts
@@ -0,0 +1,25 @@
+﻿
+import fs  = require('fs')
+var jison = require('jison')
+
+
+var bnf = fs.readFileSync("jql.jison", "utf8");
+var parser = new jison.Parser(bnf);
+
+export function Parse(source: string) : Statement{
+    return parser.parse(source);
+}
+
+export interface WhereClause {
+    Operator: string;
+    Args: any[]
+};
+
+
+export interface Statement {
+    Select: any[];
+    From: any;
+    Where: WhereClause;
+    GroupBy: any;
+}
+
