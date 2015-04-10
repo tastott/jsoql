436d00e5f0ee2d8ff833b56a85975fce747cfd42
Tim Stott <tstott@oxfordeconomics.com>
2015-04-10 13:36:42 +0100

    Beginnings of angular app

---
+++ Code/Jsoql.Gui/Scripts/Controllers/appController.ts
@@ -0,0 +1,15 @@
+﻿interface appScope extends angular.IScope {
+    QueryText: string;
+    Execute: () => void; 
+}
+
+export class AppController {
+    constructor(private $scope: appScope) {
+        $scope.QueryText = '';
+        $scope.Execute = this.Execute;
+    }
+
+    Execute = () => {
+        console.log(this.$scope.QueryText);
+    }
+}
