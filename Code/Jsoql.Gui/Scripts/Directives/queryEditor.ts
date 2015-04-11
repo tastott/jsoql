21bd51538b5c4ac4b6ec53f0eb4f95ddeaff15c8
Tim Stott <ta.stott@googlemail.com>
2015-04-11 11:30:35 +0100

    GUI: query editor using (br)ace

---
+++ Code/Jsoql.Gui/Scripts/Directives/queryEditor.ts
@@ -0,0 +1,79 @@
+﻿import $ = require('jquery')
+
+var keywords = [
+    /select\s+/ig,
+    /\s+as\s+/ig,
+    /\s+from\s+/ig,
+    /\s+join\s+/ig,
+    /\s+on\s+/ig
+];
+
+export interface QueryEditorScope extends ng.IScope {
+    value: string;
+}
+
+export class QueryEditorDirective implements ng.IDirective {
+
+    public scope = {
+        value: '='
+    }
+
+    public templateUrl = 'Views/Directives/queryEditor.html';
+
+    public link($scope: QueryEditorScope, element: JQuery, attributes: ng.IAttributes) {
+        var editable = element.children();
+        editable.html($scope.value);
+
+        var onChange = require('debounce')(() => {
+            var html = editable.html();
+            var text = GetText(editable[0]);
+            var colouredHtml = text;
+            keywords.forEach(keyword => {
+                colouredHtml = colouredHtml.replace(keyword,(match, capture) => '<span class="jsoql-keyword">' + match + '</span>');
+            });
+            editable.html(colouredHtml);
+        }, 1000);
+
+        editable.on('input',() => {
+            onChange();
+        });
+    }
+
+}
+
+
+function GetText(node: Element): string {
+    if (node.nodeType == 3) return node.textContent;
+    else return $(node)
+        .contents()
+        .map((index, child) => GetText(child))
+        .toArray()
+        .join('');
+}
+
+document = window.document;
+var brace = require('brace')
+require('brace/mode/sql')
+require('brace/theme/ambiance')
+
+export class AceQueryEditorDirective implements ng.IDirective {
+
+    public scope = {
+        value: '='
+    }
+
+    public link($scope: QueryEditorScope, element: JQuery, attributes: ng.IAttributes) {
+        var div = $('<div class="query-editor-ace"></div>')
+            .appendTo(element)
+
+        var editor = brace.edit(div[0]);
+        editor.setTheme('ace/theme/ambiance');
+        editor.getSession().setMode('ace/mode/sql');
+
+        editor.setValue($scope.value);
+        editor.getSession().on('change', function (e) {
+            $scope.value = editor.getValue();
+        });
+    }
+
+}
