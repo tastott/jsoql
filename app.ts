ccf1cf44ee5d64ee9b66bb6038f2ed905a6a81cc
Tim Stott <ta.stott@googlemail.com>
2015-04-11 11:30:35 +0100

    GUI: query editor using (br)ace

--- app.ts
+++ app.ts
@@ -2,11 +2,14 @@
 ///<reference path="Scripts/typings/angularjs/angular-route.d.ts" />
 
 import appCtrl = require('./Scripts/Controllers/appController')
-import qrDir = require('./Scripts/Directives/queryResultDirective')
+import qrDir = require('./Scripts/Directives/queryResult')
+import qeDir = require('./Scripts/Directives/queryEditor')
 
 angular.module('Jsoql', ['ngRoute'])
     .controller('AppController', appCtrl.AppController)
-    .directive('queryResult', () => new qrDir.QueryResultDirective())
+    .directive('queryResult',() => new qrDir.QueryResultDirective())
+    .directive('queryEditor',() => new qeDir.QueryEditorDirective())
+    .directive('queryEditorAce',() => new qeDir.AceQueryEditorDirective())
     .config(['$routeProvider', ($routeProvider: angular.route.IRouteProvider) => {
 
         $routeProvider.when('/home', {
