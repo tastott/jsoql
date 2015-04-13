c5f73c56212450ddb2d10d7590ee60d0ea518bfa
Tim Stott <ta.stott@googlemail.com>
2015-04-13 14:26:22 +0100

    Button to save query results

--- Scripts/Controllers/appController.ts
+++ Scripts/Controllers/appController.ts
@@ -1,6 +1,7 @@
 ﻿///<reference path="../typings/jsoql/jsoql.d.ts" />
 ///<reference path="../models/models.ts"/>
-
+import Q = require('q')
+import utilities = require('../utilities')
 var Jsoql: JsoqlStatic = require('../../../Jsoql/jsoql') //TODO: Replace with npm module eventually
 
 interface appScope extends angular.IScope {
@@ -8,6 +9,8 @@ interface appScope extends angular.IScope {
     QueryText: EditableText;
     QueryResult: QueryResult;
     Execute: () => void; 
+    SaveResults: () => void;
+    SaveResultsEnabled: () => boolean;
 }
 
 export class AppController {
@@ -16,6 +19,8 @@ export class AppController {
         $scope.BaseDirectory = { Value: this.GetLatest('BaseDirectory.Value') || process.cwd() };
         $scope.QueryText = { Value: this.GetLatest('QueryText.Value') || '' };
         $scope.Execute = this.Execute;
+        $scope.SaveResults = this.SaveResults;
+        $scope.SaveResultsEnabled = this.SaveResultsEnabled;
         $scope.QueryResult = {};
 
         this.StoreLatest('QueryText.Value');
@@ -49,5 +54,27 @@ export class AppController {
                     this.$scope.$apply(() => this.$scope.QueryResult = result); //TOOD: Better way to do this?
                 });
         }
+    }
+
+    SaveResults = () => {
+        if (this.SaveResultsEnabled()) {
+
+            //DESKTOP-ONLY
+            utilities.ShowSaveFileDialog({ InitialFilename: 'results.json' })
+                .then(path => {
+                    var json = JSON.stringify(this.$scope.QueryResult.Results, null, 4);
+                    return Q.denodeify(require('fs').writeFile)(path, json);
+                })
+                .then(() => {
+                    console.log('file saved');
+                })
+                .fail(error => {
+                    console.log(error);
+                })
+        }
+    }
+
+    SaveResultsEnabled = () => {
+        return !!this.$scope.QueryResult && !!this.$scope.QueryResult.Results;
     }
 }
