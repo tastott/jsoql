a41b61dbcc1611b521acdc6aba709ad58c9b8b84
Tim Stott <ta.stott@googlemail.com>
2015-04-17 07:31:56 +0100

    Missed some saves from previous commit

--- Code/Jsoql.Tests/lazyJsonTests.ts
+++ Code/Jsoql.Tests/lazyJsonTests.ts
@@ -4,7 +4,3 @@ export function Test1() {
     assert.ok(true, "This shouldn't fail");
 }
 
-export function Test2() {
-    assert.ok(1 === 1, "This shouldn't fail");
-    assert.ok(false, "This should fail");
-}
