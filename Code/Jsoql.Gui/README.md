aa09048098125fc1064e3bb6965644da661e030b
Tim Stott <ta.stott@googlemail.com>
2015-05-13 22:05:03 +0100

    Browser version of GUI is pretty much working (#31). Just need to figure out how to switch js files and run the browserify step automatically

--- Code/Jsoql.Gui/README.md
+++ Code/Jsoql.Gui/README.md
@@ -1,3 +1,6 @@
 ﻿# Jsoql.Gui
+## Browser-mode hacks
+### Lazy.js
 
+* `package.json` *browser* property must be set to `lazy.node.js`
 
---
+++ Code/Jsoql.Gui/server.ts
@@ -0,0 +1,14 @@
+﻿import http = require('http')
+var _static = require('node-static')
+
+var port = process.env.port || 8081;
+var fileServer = new _static.Server();
+
+
+http.createServer((req, res) => {
+    req.addListener('end', function () {
+        fileServer.serve(req, res);
+    });
+
+    req.resume();
+}).listen(port);
